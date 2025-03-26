73de20b98af01e76ebf9433e4677a5daa1b92e01
Geoffrey Wells <Gwells625@gmail.com>
2025-03-26 17:23:04 -0400

    initialize and interpolate variables

--- VariablesExercise/Program.cs
+++ VariablesExercise/Program.cs
@@ -4,7 +4,15 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string myName = "Geoff";
+            int myAge = 31;
+            double height = 5.11;
+            decimal weight = 185.5m;
+            char gender = 'M';
+            bool married = true;
+            
+            Console.WriteLine($"Name: {myName}\nAge: {myAge}\nHeight:  {height}\nWeight: {weight}\nGender: {gender}\nMarried: {married} ");
+
         }
     }
 }
